105af95ce1469d25f2019b8a7dac13627c32bf8f
noasnr0406 <ktc24a32j0001@edu.kyoto-tech.ac.jp>
2024-10-19 12:38:27 +0900

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM  from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import '.index.css';
-ReactDOM. createRoot (document.getElementById('root') as HTMLElement).render(
+import './index.css';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-     </BrowserRouter> 
-    </React.StrictMode>
-); 
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
